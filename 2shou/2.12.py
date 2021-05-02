0f9c8eefb53c4a5915200b57aab64f799de24156
yanamt <53215000+yanamt@users.noreply.github.com>
2021-05-02 21:43:57 +0900

    Update 2.12.py

--- 2shou/2.12.py
+++ 2shou/2.12.py
@@ -3,5 +3,5 @@ import pandas as pd
 df=pd.read_csv("popular-names.txt",sep="\t",header=None)
 col1=df.iloc[:,0]
 col2=df.iloc[:,1]
-col1.to_csv("col1.txt",header=None)
-col2.to_csv("col2.txt",header=None)
+col1.to_csv("col1.txt",sep=" ",header=None,index=False)
+col2.to_csv("col2.txt",sep=" ",header=None,index=False)
