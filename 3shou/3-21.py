a429b16172581c87c84d426bfa8a060c402b2e2e
yanamt <53215000+yanamt@users.noreply.github.com>
2021-05-15 15:32:44 +0900

    Create 3-21.py

---
+++ 3shou/3-21.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import re
+
+pat=re.compile("Category")
+df = pd.read_json("jawiki-country.json",orient="records",lines=True)
+british = df[df["title"]=="イギリス"]["text"].values
+
+lines_list = british[0].split("\n")
+
+for line in lines_list:
+    if re.search(pat,line):
+        print(line)
