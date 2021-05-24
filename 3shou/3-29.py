5cdb42e2a671c4512373cca33eb8382320217303
yanamt <53215000+yanamt@users.noreply.github.com>
2021-05-24 22:35:31 +0900

    Add files via upload

---
+++ 3shou/3-29.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import re
+import requests
+
+country = pd.read_json("jawiki-country.json", orient="records", lines=True)
+britain = country[country["title"] == "イギリス"]["text"].values
+
+text_lines = britain[0].split("\n")
+
+pat = re.compile("\|(.+?)\s=\s*(.+)")
+
+d = {}
+
+for line in text_lines:
+    ans = re.search(pat, line)
+    if ans:
+        d[ans[1]] = ans[2]
+
+S = requests.Session()
+URL = "https://commons.wikimedia.org/w/api.php"
+PARAMS = {
+    "action": "query",
+    "format": "json",
+    "titles": "File:" + d["国旗画像"],
+    "prop": "imageinfo",
+    "liprop": "url"
+}
+R = S.get(url=URL, params=PARAMS)
+DATA = R.json()
+PAGES = DATA["query"]["pages"]
+for k, v in PAGES.items():
+    print(v["imageinfo"][0]["url"])
