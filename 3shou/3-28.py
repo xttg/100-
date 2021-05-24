fbd7a4793baf0525e5bb8e1e81580f13f79cd329
yanamt <53215000+yanamt@users.noreply.github.com>
2021-05-24 16:59:19 +0900

    Add files via upload

---
+++ 3shou/3-28.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import re
+
+country = pd.read_json("jawiki-country.json", orient="records", lines=True)
+britain = country[country["title"] == "イギリス"]["text"].values
+
+text_lines = britain[0].split("\n")
+
+pat = re.compile("\|(.+?)\s=\s*(.+)")
+empha = re.compile("\‘{2,5}(.+?)\’{2,5}")
+link = re.compile("\[\[(.+?)\]\]")
+html = re.compile("<(.+?)>")
+
+
+d = {}
+
+for line in text_lines:
+    ans = re.search(pat, line)
+    if ans:
+        d[ans[1]] = ans[2]
+    text = re.sub(empha, "\\1", line)
+    text1 = re.sub(link, "\\1")
+    text2 = re.sub(html, "", text1)
+print(d)
