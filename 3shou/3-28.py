3af3e691bf0015d83b38e14994abe187a881c263
yanamt <53215000+yanamt@users.noreply.github.com>
2021-05-30 16:39:04 +0900

    Update 3-28.py

--- 3shou/3-28.py
+++ 3shou/3-28.py
@@ -6,10 +6,10 @@ britain = country[country["title"] == "イギリス"]["text"].values
 
 text_lines = britain[0].split("\n")
 
-pat = re.compile("\|(.+?)\s=\s*(.+)")
-empha = re.compile("\‘{2,5}(.+?)\’{2,5}")
-link = re.compile("\[\[(.+?)\]\]")
-html = re.compile("<(.+?)>")
+pat = re.compile(r"\|(.+?)\s=\s*(.+)")
+empha = re.compile(r"\'{2,5}(.+?)\'{2,5}")
+link = re.compile(r"\[\[(.+?)\]\]")
+html = re.compile(r"<(.+?)>")
 
 
 d = {}
@@ -19,7 +19,7 @@ for line in text_lines:
     if ans:
         d[ans[1]] = ans[2]
     text = re.sub(empha, "\\1", line)
-    text1 = re.sub(link, "\\1")
-    text2 = re.sub(html, "\\1", text1)
+    text1 = re.sub(link, "\\1", text)
+    text2 = re.sub(html, "", text1)
+    print(text2)
 print(d)
-
