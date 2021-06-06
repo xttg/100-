195afe681bc0d0b50b1240fe18666a018caadbe8
yanamt <53215000+yanamt@users.noreply.github.com>
2021-06-06 13:12:53 +0900

    Add files via upload

---
+++ 4shou/4-33.py
@@ -0,0 +1,19 @@
+import MeCab
+with open("neko.txt.mecab", "r") as f:
+    l = []
+    noun = []
+    for line in f:
+        if line == "EOS\n":
+            continue
+        else:
+            l1 = line.split("\t")
+            if len(l1) != 2:
+                continue
+            l2 = l1[1].split(",")
+            d = {"surface": l1[0], "base": l2[6], "pos": l2[0], "pos1": l2[1]}
+            l.append(d)
+    for i in range(1, len(l)-1):
+        if l[i-1]["pos"] == "名詞" and l[i]["surface"] == "の" and l[i+1]["pos"] == "名詞":
+            noun.append(l[i-1]["surface"]+l[i]
+                        ["surface"]+l[i+1]["surface"])
+    print(noun)
